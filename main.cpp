2e821b52e37d0c888190f9dc40484ef4fd635b4e
Giveonaldo <giveonaldo@gmail.com>
2022-12-31 09:59:33 +0700

    create void function gotoxy

--- main.cpp
+++ main.cpp
@@ -176,6 +176,16 @@ void password()
 	}
 }
 
+void gotoxy(int x, int y)
+{
+	COORD coord;
+	coord.X = x;
+	coord.Y = y;
+
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
+
+
 int main()
 {
 
