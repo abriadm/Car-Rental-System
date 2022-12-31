360a510f3a7e3e201b2aacc49c40607d4794fa38
Giveonaldo <giveonaldo@gmail.com>
2022-12-31 09:21:33 +0700

    create struct car

--- main.cpp
+++ main.cpp
@@ -9,7 +9,18 @@
 
 using namespace std;
 
-
+struct car
+{
+	char plate_num[10];
+	char brand[20];
+	char model[20];
+	float capacity;
+	char colour[20];
+	float rate_per_hour;
+	float rate_per_half;
+	float rate_per_day;
+	char transmission[6];
+};
 
 int main()
 {
