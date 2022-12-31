daeb488f6f963e328738463f59bfc43d361a0549
Giveonaldo <giveonaldo@gmail.com>
2022-12-31 09:17:40 +0700

    import library

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include <conio.h>
+#include <fstream>
+#include <Windows.h>
+#include <string.h>
+#include <stdio.h>
+#include <time.h>
+#include <cstdlib>
 
 using namespace std;
+
+
 
 int main()
 {
